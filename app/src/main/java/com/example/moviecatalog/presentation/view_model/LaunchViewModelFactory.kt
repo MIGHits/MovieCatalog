6db8928f3351dfff6acc2e04f6a220eee2755b52
MIGHits <164134306+MIGHits@users.noreply.github.com>
2024-11-07 05:16:31 +0700

    hot fix addDatabaseUser after logout->login

--- app/src/main/java/com/example/moviecatalog/presentation/view_model/LaunchViewModelFactory.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/view_model/LaunchViewModelFactory.kt
@@ -2,11 +2,17 @@ package com.example.moviecatalog.presentation.view_model
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.moviecatalog.data.data.mappers.BanedMoviesMapper
+import com.example.moviecatalog.data.data.mappers.DbFriendsMapper
+import com.example.moviecatalog.data.data.mappers.DbGenresToDomain
+import com.example.moviecatalog.data.data.mappers.DbUserMapper
 import com.example.moviecatalog.data.data.mappers.ProfileDTOMapper
 import com.example.moviecatalog.data.data.mappers.ProfileMapper
 import com.example.moviecatalog.data.data.remote.dataSource.UserServiceProvider
+import com.example.moviecatalog.data.data.repository.DatabaseRepositoryImpl
 import com.example.moviecatalog.data.data.repository.UserRepositoryImpl
 import com.example.moviecatalog.data.data.storage.PrefsTokenStorage
+import com.example.moviecatalog.domain.usecase.AddUserDbUseCase
 import com.example.moviecatalog.domain.usecase.GetUserProfileDataUseCase
 import com.example.moviecatalog.domain.usecase.IsUserLoginUseCase
 import com.example.moviecatalog.presentation.mappers.ProfileUIMapper
@@ -21,7 +27,15 @@ class LaunchViewModelFactory : ViewModelProvider.Factory {
                     ProfileMapper(),
                     ProfileDTOMapper()
                 )
-            ), ProfileUIMapper()
+            ), ProfileUIMapper(),
+            AddUserDbUseCase(
+                DatabaseRepositoryImpl(
+                    DbUserMapper(),
+                    DbFriendsMapper(),
+                    DbGenresToDomain(),
+                    BanedMoviesMapper()
+                )
+            )
         ) as T
     }
 }
